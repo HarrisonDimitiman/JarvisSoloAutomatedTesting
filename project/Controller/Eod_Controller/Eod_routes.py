55fd571beb5883da4a19b583a7e776a20ab2225f
Adrian <eydre@outlook.com>
2022-07-05 23:06:00 +0800

    add classes with different getvalue implementation

--- project/Controller/Eod_Controller/Eod_routes.py
+++ project/Controller/Eod_Controller/Eod_routes.py
@@ -1,23 +1,101 @@
+from crypt import methods
 from flask import Blueprint, render_template, url_for, request, redirect
 from flask_login import login_required, current_user
-from Project.Controller.Eod_Controller.Eod_tester import testEod
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from Project.Controller.Global_Controller.Global_test import Login
+from .Eod_xpath import EodXpath, BreakdownXpath, EodSetupXpath, ModalMetricXpath
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 eod = Blueprint('eod', __name__)
 
-@eod.route("/eod-form")
+@eod.route("/eod-form", methods=["POST", "GET"])
 def eodForm():
-    d = DesiredCapabilities.CHROME
-    d['loggingPrefs'] = {'browser': 'ERROR'}
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), desired_capabilities=d)
-    Login.login(driver, "https://solo.next.jarvisanalytics.com/end-of-day", "testryan", "Jarvis.123")
-    
-    if not driver:
-        return False
+    if request.method == 'POST':
+        print(request.form)
 
-    testEod(driver)
+        d = DesiredCapabilities.CHROME
+        d['loggingPrefs'] = {'browser': 'ERROR'}
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), desired_capabilities=d)
+        Login.login(driver, "https://solo.next.jarvisanalytics.com/end-of-day", "testryan", "Jarvis.123")
+        
+        if not driver:
+            return False
+
+        testEod(driver)
     return render_template('Eod_Template/Eod_index.html')
+
+def testEod(driver):
+    result = {}
+
+    try:
+        date_picker = WebDriverWait(driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, EodSetupXpath.date_picker))
+        )
+
+        xpaths = getXpath()
+
+        for metric in xpaths:
+            # print(xpaths[metric]["main"])
+
+            result[metric] = {}
+            # result[metric]["main"] = driver.find_element(
+            #     by = By.XPATH,
+            #     value = xpaths[metric]["main"]
+            # ).get_attribute("value")
+            xpaths[metric]["main"].instantiateElement(driver)
+
+            result[metric]["main"] = xpaths[metric]["main"].getValue()
+
+            # breakdown_btn = driver.find_element(
+            #     by = By.XPATH,
+            #     value = xpaths[metric]["breakdown"]["xpath"]
+            # )
+            # breakdown_btn.click()
+            xpaths[metric]["breakdown"]["button"].instantiateElement(driver)
+            xpaths[metric]["breakdown"]["button"].click()
+
+            xpaths[metric]["breakdown"]["metric"].instantiateElement(driver)
+            result[metric]["breakdown"] = xpaths[metric]["breakdown"]["metric"].getValue()
+
+            # web_element = WebDriverWait(driver, 60).until(
+            #     EC.presence_of_element_located((By.XPATH, ))
+            # )
+        print(result)
+
+
+        
+    except Exception as e:
+        print(f"Some error in here {e}")
+    finally:
+        driver.quit()
+        return result
+
+def getXpath():
+    return {
+        "collection": getMainBreakdown(EodXpath.collection, BreakdownXpath.collection, ModalMetricXpath.collection),
+        "adjustments": getMainBreakdown(EodXpath.adjustments, BreakdownXpath.adjustments, ModalMetricXpath.adjustments),
+        "case_acceptance": getMainBreakdown(EodXpath.case_acceptance, BreakdownXpath.case_acceptance, ModalMetricXpath.case_acceptance),
+        "missing_ref": getMainBreakdown(EodXpath.missing_ref, BreakdownXpath.missing_ref, ModalMetricXpath.missing_ref),
+        "no_show": getMainBreakdown(EodXpath.no_show, BreakdownXpath.no_show, ModalMetricXpath.no_show),
+        "daily_coll": getMainBreakdown(EodXpath.daily_coll, BreakdownXpath.daily_coll, ModalMetricXpath.daily_coll),
+        "hyg_reapp": getMainBreakdown(EodXpath.hyg_reapp, BreakdownXpath.hyg_reapp, ModalMetricXpath.hyg_reapp),
+        "new_patients": getMainBreakdown(EodXpath.new_patients, BreakdownXpath.new_patients, ModalMetricXpath.new_patients),
+        "same_day_treat": getMainBreakdown(EodXpath.same_day_treat, BreakdownXpath.same_day_treat, ModalMetricXpath.same_day_treat),
+        "pt_portion": getMainBreakdown(EodXpath.pt_portion, BreakdownXpath.pt_portion, ModalMetricXpath.pt_portion)
+    }
+
+def getMainBreakdown(main, button, breakdown):
+    return {
+        "main": main,
+        "breakdown": {
+            "button": button,
+            "metric": breakdown
+        }
+    }
+
+
--- project/Controller/Eod_Controller/Eod_xpath.py
+++ project/Controller/Eod_Controller/Eod_xpath.py
@@ -1,28 +1,113 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Element:
+    def __init__(self, name, xpath, type):
+        self.element = None
+        self.name = name
+        self.xpath = xpath
+        self.type = type
+        self.instantiated = False
+
+    def instantiateElement(self, driver):
+        self.instantiated = True
+        try:
+            self.element = WebDriverWait(driver, 60).until(
+                EC.presence_of_element_located((By.XPATH, self.xpath))
+            )
+        except: 
+            pass
+    
+    def getElement(self):
+        return self.element
+
+# InputElement, CollectionElement, and TextElement implements different getValue().
+# Message to future eyd: Need to find way to require the implementation of the getValue method of children classes inherting the Element class.
+
+class InputElement(Element):
+    def __init__(self, name, xpath):
+        super().__init__(name, xpath, "value")
+    
+    def getValue(self):
+        if self.element == None: 
+            raise Exception("Element not instantiated.")
+
+        return self.element.get_attribute('value')
+
+class TextElement(Element):
+    def __init__(self, name, xpath):
+        super().__init__(name, xpath, "value")
+    
+    def getValue(self):
+        if self.element == None: 
+            raise Exception("Element not instantiated.")
+
+        return self.element.text
+
+class CollectionElement(Element):
+    def __init__(self, name, xpath):
+        super().__init__(name, xpath, "collection")
+
+    def getValue(self):
+        if self.element == None: 
+            raise Exception("Element not instantiated.")
+
+        return len(self.element)
+
+class ClickableElement(Element):
+    def __init__(self, name, xpath):
+        super().__init__(name, xpath, "button")
+
+    def click(self):
+        if self.element == None: 
+            raise Exception("Element not instantiated.")
+        
+        self.element.click()
+
+# BEYOND ARE HARDCODED XPATHS, NEED TO CHANGE IMPLEMENTATION LATER TO INCOPORATE DATABASE CALLS
+
 class EodXpath:
-    collection = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[13]/div[2]/input'
-    adjustments = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[12]/div[2]/input'
-    case_acceptance = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[20]/div[2]/input'
-    missing_ref = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[24]/div[2]/input'
-    no_show = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[17]/div[2]/input'
-    daily_coll = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[14]/div[2]/input'
-    hyg_reapp = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[28]/div[2]/input'
-    new_patients = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[21]/div[2]/input'
-    same_day_treat = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[18]/div[2]/input'
-    pt_portion = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[5]/div[2]/input'
+    collection = InputElement('collection','/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[13]/div[2]/input')
+    adjustments = InputElement('adjustments', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[12]/div[2]/input')
+    case_acceptance = InputElement('case_acceptance','/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[20]/div[2]/input')
+    missing_ref = InputElement('missing_ref', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[24]/div[2]/input')
+    no_show = InputElement('no_show', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[17]/div[2]/input')
+    daily_coll = InputElement('daily_coll', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[14]/div[2]/input')
+    hyg_reapp = InputElement('hyg_reapp', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[28]/div[2]/input')
+    new_patients = InputElement('new_patients', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[21]/div[2]/input')
+    same_day_treat = InputElement('same_day_treat', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[18]/div[2]/input')
+    pt_portion = InputElement('pt_portion', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[5]/div[2]/input')
 
 class BreakdownXpath:
-    collection = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[13]/div[1]/div/span/a'
-    adjustments = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[12]/div[1]/div/span/a'
-    case_acceptance = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[20]/div[1]/label/span/a'
-    missing_ref = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[24]/div[1]/label/div/a'
-    no_show = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[17]/div[1]/label/div/a'
-    daily_coll = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[14]/div[1]/label/div/a'
-    hyg_reapp = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[28]/div[1]/label/div/a'
-    new_patients = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[2]/div[12]/div[1]/label/div/a'
-    same_day_treat = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[18]/div[1]/label/div/a'
-    pt_portion = '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[5]/div[1]/label/div/a'
+    collection = ClickableElement('collection', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[13]/div[1]/div/span/a')
+    adjustments = ClickableElement('adjustments', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[12]/div[1]/div/span/a')
+    case_acceptance = ClickableElement('case_acceptance', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[20]/div[1]/label/span/a')
+    missing_ref = ClickableElement('missing_ref', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[24]/div[1]/label/div/a')
+    no_show = ClickableElement('no_show', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[17]/div[1]/label/div/a')
+    daily_coll = ClickableElement('daily_coll', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[14]/div[1]/label/div/a')
+    hyg_reapp = ClickableElement('hyg_reapp', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[28]/div[1]/label/div/a')
+    new_patients = ClickableElement('new_patients', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[2]/div[12]/div[1]/label/div/a')
+    same_day_treat = ClickableElement('same_day_treat', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[18]/div[1]/label/div/a')
+    pt_portion = ClickableElement('pt_portion', '/html/body/div[1]/main/div[2]/div[3]/div[1]/div/form/div/div[1]/div[1]/div/div[5]/div[1]/label/div/a')
+
+# june 21 and some other dates for missing ref and new patients
+class ModalMetricXpath:
+    collection = TextElement('collection', '/html/body/div[1]/main/div[3]/div[9]/div/div/div[3]/div[1]/div/table/tfoot/tr/td[6]')
+    adjustments = TextElement('adjustments', '/html/body/div[1]/main/div[3]/div[2]/div/div/div[3]/div[1]/div/table/tfoot/tr/td[6]')
+    case_acceptance = TextElement('case_acceptance', '/html/body/div[1]/main/div[3]/div[9]/div/div/div[2]/div[1]/div/table/tfoot/tr[1]/td[6]')
+    # missing ref is number of rows
+    missing_ref = CollectionElement('missing_ref', '/html/body/div[1]/main/div[3]/div[7]/div/div/div[2]/div[2]/div/table/tbody/tr')
+    no_show = TextElement('no_show', '/html/body/div[1]/main/div[3]/div[4]/div/div/div[2]/div[2]/div/table/tr/td[2]/strong/span')
+    daily_coll = TextElement('daily_coll', '/html/body/div[1]/main/div[3]/div[3]/div/div/div[2]/div[2]/div/table/tr/td[4]/strong/span')
+    hyg_reapp = TextElement('hyg_reapp', '/html/body/div[1]/main/div[3]/div[8]/div/div/div[2]/div[2]/div/table/tr/td[3]/strong/span')
+    # new patients is number of rows
+    new_patients = CollectionElement('new_patients', '/html/body/div[1]/main/div[3]/div[6]/div/div/div[2]/table/tbody/tr')
+    same_day_treat = TextElement('same_day_treat', '/html/body/div[1]/main/div[3]/div[5]/div/div/div[2]/div[2]/div/table/tr[2]/td[5]/strong/span')
+    pt_portion = TextElement('pt_portion', '/html/body/div[1]/main/div[3]/div[1]/div/div/div[2]/div[2]/div/table/tr[1]/td[2]/strong/span')
 
 class EodSetupXpath:
     date_picker = '//*[@class="vue-daterange-picker w-full"]'
     location_picker = '/html/body/div[1]/main/div[1]/div/div/div/div[2]/div/div[2]'
     refresh_btn = '/html/body/div[1]/main/div[1]/div/div/div/div[3]/button'
+
